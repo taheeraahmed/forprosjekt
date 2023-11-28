0b1d55163b56810561fc275e096a6aab02228222
Taheera Ahmed <taheera@hotmail.com>
2023-11-28 10:02:56 +0100

    Update training script and code path

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DATE=$(date +%Y%m%d-%H%M%S)
 USER=$(whoami)
-TYPE="tensorflow-binary-classification-test"
+TYPE="tensorflow-binary-classification-longer-test"
 echo "Current USER is: $USER"
 
 ID=${DATE}-${TYPE}
@@ -9,13 +9,30 @@ echo "Current ID is: $ID"
 OUTPUT_FILE="/cluster/home/taheeraa/code/forprosjekt/idun/$ID.out"
 echo "Current OUTPUT_FILE is: $ID"
 
+# Define the destination path for the code
+CODE_PATH="/cluster/home/$USER/runs/code/${ID}"
+
+# Copy the code with rsync, excluding .venv
+echo "Copying code to $CODE_PATH"
+mkdir -p $CODE_PATH
+rsync -av \
+  --exclude='.venv' \
+  --exclude='idun' \
+  --exclude='images' \
+  --exclude='runs' \
+  --exclude='scripts' \
+  --exclude='.git' \
+  --exclude='__pycache' \
+  /cluster/home/$USER/code/tdt17-visuell-intelligens/ $CODE_PATH
+
+echo "Running slurm job from $CODE_PATH"
 sbatch --partition=GPUQ \
   --account=ie-idi \
-  --time=08:15:00 \
+  --time=28:15:00 \
   --nodes=1 \
   --ntasks-per-node=1 \
   --mem=50G \
   --gres=gpu:1 \
   --job-name=$ID \
   --output=$OUTPUT_FILE \
-  ./train.slurm
+  $CODE_PATH/train.slurm
