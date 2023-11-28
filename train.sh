9e2ad4624f0f275c654f15db276fe9c7120be0bc
Taheera Ahmed <taheera@hotmail.com>
2023-11-28 11:18:16 +0100

    Add test mode and plots of val and train loss

--- train.sh
+++ train.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 DATE=$(date +%Y%m%d-%H%M%S)
 USER=$(whoami)
-TYPE="tensorflow-binary-classification-longer-test"
+JOB_NAME="tensorflow-binary-classification-longer"
 CURRENT_PATH=$(pwd)
-echo "Current USER is: $USER"
-echo "Current path is: $CURRENT_PATH"
+TEST_MODE=true
 
-ID=${DATE}-${TYPE}
+# Check if TEST_MODE is true and append "test" to JOB_NAME
+if [ "$TEST_MODE" = true ]; then
+    JOB_NAME="${JOB_NAME}-test"
+fi
+
+# 
+ID=${DATE}-${JOB_NAME}
 echo "Current ID is: $ID"
 OUTPUT_FILE="/cluster/home/taheeraa/code/forprosjekt/idun/$ID.out"
 echo "Current OUTPUT_FILE is: $ID"
@@ -27,16 +32,21 @@ rsync -av \
   --exclude='.git' \
   --exclude='__pycache' \
   --exclude='utils/__pycache' \
+  --exclude='mlruns/' \
   /cluster/home/$USER/code/forprosjekt/ $CODE_PATH
 
+echo "Current USER is: $USER"
+echo "Current path is: $CURRENT_PATH"
+echo "Current mode is: $JOB_NAME"
 echo "Running slurm job from $CODE_PATH"
 sbatch --partition=GPUQ \
   --account=ie-idi \
-  --time=28:15:00 \
+  --time=0:15:00 \
   --nodes=1 \
   --ntasks-per-node=1 \
   --mem=50G \
   --gres=gpu:1 \
   --job-name=$ID \
   --output=$OUTPUT_FILE \
+  --export=TEST_MODE=$TEST_MODE \
   $CODE_PATH/train.slurm
--- train_pytorch.py
+++ train_pytorch.py
@@ -5,6 +5,9 @@ from models import DenseNetBinaryClassifier
 from torchvision import transforms
 from tqdm import tqdm
 import math
+import argparse
+import matplotlib.pyplot as plt
+import time
 
 import torch
 import torch.nn as nn
@@ -18,20 +21,23 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 TODO: 
 [x] Add test mode
 [ ] Add plots of train and val accuracy
-[ ] Add plots of train and val loss 
+[X] Add plots of train and val loss 
 [ ] Log f1 scores
 """
 
-def train(test = True):
+def train(args):
+    start_time = time.time()
+    test = args.test
+
     logger = set_up()
     test_size = 0.2
+    resize_size = (256, 256)
+
     if test:
         logger.warning(f'In test mode')
-        resize_size = (256, 256)
-        num_epochs = 1
+        num_epochs = 20
         batch_size = 3
     else:  
-        resize_size = (32, 32)
         num_epochs= 100
         batch_size = 32
 
@@ -66,6 +72,8 @@ def train(test = True):
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     logger.info('Started training')
+    train_losses = []
+    val_losses = []
 
     for epoch in range(num_epochs):
         #Training phase
@@ -81,12 +89,12 @@ def train(test = True):
             optimizer.step()
 
             # Update the progress bar with the loss information
-            train_progress_bar.set_postfix(loss=loss.item())
+            train_progress_bar.set_postfix(train_loss=loss.item())
             train_loss += loss.item()
 
         train_loss /= len(train_loader)
+        train_losses.append(train_loss)
 
-        # Calculate and log training metrics
         # Validation phase
         model.eval()  # Set the model to evaluation mode
         val_loss = 0.0
@@ -97,12 +105,30 @@ def train(test = True):
                 outputs = model(inputs)
                 outputs = outputs.squeeze(1)
                 loss = criterion(outputs, labels)
-                val_progress_bar.set_postfix(loss=loss.item())
+                val_progress_bar.set_postfix(val_loss=loss.item())
 
                 val_loss += loss.item()
         
         val_loss /= len(val_loader)
-    
+        val_losses.append(val_loss)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    logger.info(f"Elapsed time: {elapsed_time} seconds")
+
+    images_path = 'images/' 
+    plt.figure(figsize=(10, 5))
+    plt.plot(train_losses, label='Training Loss')
+    plt.plot(val_losses, label='Validation Loss')
+    plt.title('Training and Validation Losses Per Epoch')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig(f'{images_path}train_val_loss.png')
+    logger.info(f'Saved images to: {images_path}train_val_loss.png')
 
 if __name__ == "__main__":
-    train()
+    parser = argparse.ArgumentParser(description="Arguments for training with pytorch")
+    parser.add_argument("-t", "--test", help="Test mode?", default=False, required=True)
+    args = parser.parse_args()
+    train(args)
