1508ca6e6cd8334527e7f2166fd1589d6f4bace3
taheeraahmed <taheera@hotmail.com>
2023-12-11 14:59:55 +0100

    Add handling of underrepresented diseases

--- src/datasets/multi_class.py
+++ src/datasets/multi_class.py
@@ -1,11 +1,10 @@
 import os
 from PIL import Image
 import torch
-import PIL.Image as Image
-
+from torchvision import transforms
 from torch.utils.data import Dataset
 
-class ChestXrayMultiClassDataset(Dataset):
+class ChestXray14MultiClassDataset(Dataset):
     def __init__(self, dataframe, transform=None):
         """
         Args:
@@ -14,6 +13,28 @@ class ChestXrayMultiClassDataset(Dataset):
         """
         self.dataframe = dataframe
         self.transform = transform
+        self.labels = ['Atelectasis', 'Cardiomegaly', 'Consolidation', 'Edema', 'Effusion', 'Emphysema',
+                       'Fibrosis', 'Hernia', 'Infiltration', 'Mass', 'Nodule', 'Pleural_Thickening',
+                       'Pneumonia', 'Pneumothorax']
+
+        underrepresented_diseases = ['Hernia', 'Pneumonia', 'Fibrosis', 'Emphysema', 'Cardiomegaly', 'Pleural_Thickening', 'Consolidation', 'Pneumothorax', 'Mass', 'Nodule']
+
+        self.data_augmentation = transforms.Compose([
+            transforms.RandomRotation(0.1),
+            transforms.RandomResizedCrop((224,224), scale=(0.8, 1.0)),
+            transforms.RandomHorizontalFlip(),
+        ])
+
+        self.underrepresented_diseases_indices = [self.labels.index(disease) for disease in underrepresented_diseases]
+
+    def _augment_images(self, image, labels):
+        ''' 
+        Augments the images of the underrepresented diseases.
+        '''
+        for i in self.underrepresented_diseases_indices:
+            if labels[i] == 1:
+                return self.data_augmentation(image), labels
+        return image, labels
 
     def __len__(self):
         return len(self.dataframe)
@@ -23,12 +44,18 @@ class ChestXrayMultiClassDataset(Dataset):
             idx = idx.tolist()
 
         img_path = self.dataframe.iloc[idx, 0]
-        image = Image.open(img_path).convert('RGB')
+        try:
+            image = Image.open(img_path).convert('RGB')
+        except (IOError, FileNotFoundError):
+            print(f"Error opening image file: {img_path}")
+            return None
+
         labels = self.dataframe.iloc[idx, 1:].to_numpy()
         labels = torch.from_numpy(labels.astype('float32'))
+
+        image, labels = self._augment_images(image, labels)
 
         if self.transform:
             image = self.transform(image)
 
         return {"img": image, "lab": labels}
-
